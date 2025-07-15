1301e64405e465c826bc5cc3b1b0a81ddffd7ef6
Kristina <31845754+HopelessCompass@users.noreply.github.com>
2025-07-15 15:21:53 +0300

    Daemon work

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import time
+import signal
+import sys
+import daemon
 from collector import load_config, collect_logs
 
-if __name__ == "__main__":
+RUNNING = True
+
+def handle_exit(signum, frame):
+    global RUNNING
+    RUNNING = False
+    print("⛔ Получен сигнал завершения, выходим...")
+
+def run_daemon(interval_seconds=30):
     config = load_config()
-    collect_logs(config)
+
+    signal.signal(signal.SIGTERM, handle_exit)
+    signal.signal(signal.SIGINT, handle_exit)
+
+    while RUNNING:
+        collect_logs(config)
+        time.sleep(interval_seconds)
+
+if __name__ == "__main__":
+    with daemon.DaemonContext():
+        run_daemon()
